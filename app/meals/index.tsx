e083dc9cf75b64a7cfb6a890b4310ccb6ce5531a
tamasstephen <kicsitnagyon@gmail.com>
2024-12-30 19:11:38 +0100

    style: add card styles

--- app/meals/index.tsx
+++ app/meals/index.tsx
@@ -1,5 +1,12 @@
 import { MEALS } from "@/data/dummy-data";
-import { Pressable, FlatList, View, StyleSheet, Text } from "react-native";
+import {
+  Image,
+  Pressable,
+  FlatList,
+  View,
+  StyleSheet,
+  Text,
+} from "react-native";
 import Title from "@/components/Title";
 import { Href, router, useLocalSearchParams } from "expo-router";
 import Meal from "@/models/meal";
@@ -18,7 +25,13 @@ const MealItem = ({ meal }: { meal: Meal }) => {
       onPress={() => router.push(`/meals/${meal.id}` as Href)}
     >
       <View>
+        <Image source={{ uri: meal.imageUrl }} style={styles.image} />
         <Text style={styles.itemText}>{meal.title}</Text>
+        <View style={styles.tagContainer}>
+          <Text style={styles.tag}>{meal.duration} min</Text>
+          <Text style={styles.tag}>{meal.complexity}</Text>
+          <Text style={styles.tag}>{meal.affordability}</Text>
+        </View>
       </View>
     </Pressable>
   );
@@ -37,8 +50,7 @@ export default function Overview() {
         data={availableMeals}
         renderItem={({ item }) => <MealItem meal={item} />}
         keyExtractor={(item) => item.id}
-        style={{ width: "100%", maxHeight: "50%", marginTop: 32 }}
-        numColumns={1}
+        style={styles.itemList}
       />
     </View>
   );
@@ -46,23 +58,56 @@ export default function Overview() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 32,
+    marginVertical: 32,
+    width: "100%",
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+  },
+  itemList: {
+    width: "80%",
+    maxHeight: "100%",
+    marginTop: 32,
   },
   itemText: {
     color: "white",
     textAlign: "center",
+    paddingTop: 16,
+    fontWeight: "bold",
+  },
+  tagContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingBottom: 16,
+    paddingTop: 8,
+  },
+  tag: {
+    color: "white",
+    textAlign: "center",
+    padding: 8,
+    fontSize: 12,
+    borderColor: "#d4d4d4",
+    borderRadius: 4,
+    margin: 4,
+    backgroundColor: "#323232",
+    borderWidth: 1,
   },
   itemContainer: {
-    padding: 8,
     margin: 8,
     borderRadius: 6,
     backgroundColor: "#292929",
-    borderColor: "white",
-    borderWidth: 1,
+    overflow: "hidden",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
   buttonPressed: {
     opacity: 0.5,
+  },
+  image: {
+    objectFit: "cover",
+    aspectRatio: 1,
   },
 });
